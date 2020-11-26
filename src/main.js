83078749ab33baab846f086ad9a5d852320e3f16
jimena campos <jimena@ucol.mx>
2020-11-26 05:45:08 -0600

    esMultiplo function implemented

--- src/main.js
+++ src/main.js
@@ -10,6 +10,13 @@ export default class App {
      }
     }
 
+    esMultiplo(numero1, numero2){
+        if(numero1 % numero2 === 0){
+           return true;
+        } else {
+           return false;
+        }
+    }
     
 }
 
@@ -18,3 +25,6 @@ let app = new App();
 console.log(app.esPar(0));
 console.log(app.esPar(5));
 console.log(app.esPar(6));
+
+console.log(app.esMultiplo(144,12));
+console.log(app.esMultiplo(144,11));
