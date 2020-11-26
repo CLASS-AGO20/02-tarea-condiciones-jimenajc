e1adebd380809d94ecbb47509b615282017d97e3
jimena campos <jimena@ucol.mx>
2020-11-26 06:19:04 -0600

    obtenerCalificacion implemented

--- src/main.js
+++ src/main.js
@@ -36,6 +36,28 @@ export default class App {
         }
         return resultado;
     }
+
+    obtenerCalificacion(calificacionNumerica){
+        let calificacionLetras;
+
+        if (calificacionNumerica >= 0 && calificacionNumerica <= 5){
+             calificacionLetras = "C";
+        } else if (calificacionNumerica === 6){
+            calificacionLetras = "B";
+        } else if (calificacionNumerica === 7){
+            calificacionLetras = "B";
+        } else if (calificacionNumerica === 8){
+            calificacionLetras = "A";
+        } else if (calificacionNumerica === 9){
+            calificacionLetras = "A";
+        } else if (calificacionNumerica === 10){
+            calificacionLetras = "A+";
+        } else {
+            calificacionLetras = "ERROR";
+        }
+
+        return calificacionLetras
+    }
 }
 
 let app = new App();
@@ -51,3 +73,13 @@ console.log(app.calcularSalario(1, 2));
 console.log(app.calcularSalario(2, 2));
 console.log(app.calcularSalario(3, 2));
 console.log(app.calcularSalario(5, 2));
+
+console.log(app.obtenerCalificacion(0));
+console.log(app.obtenerCalificacion(4));
+console.log(app.obtenerCalificacion(5));
+console.log(app.obtenerCalificacion(6));
+console.log(app.obtenerCalificacion(7));
+console.log(app.obtenerCalificacion(8));
+console.log(app.obtenerCalificacion(9));
+console.log(app.obtenerCalificacion(10));
+console.log(app.obtenerCalificacion(11));
